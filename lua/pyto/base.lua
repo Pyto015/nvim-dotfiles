2fc7886a9c487bea5c58eb54bb48a5604b270ed3
Pyto015 <pyto015@ya.ru>
2022-12-15 21:24:22 +0300

    Base configuration

---
+++ lua/pyto/base.lua
@@ -0,0 +1,14 @@
+--base configuration vim
+
+local v = vim
+
+v.opt.number= true
+v.opt.swapfile = false
+v.opt.wrap = false
+v.opt.syntax = "on"
+v.opt.encoding = "utf-8"
+v.opt.expandtab = true
+v.opt.smarttab = true
+v.opt.shiftwidth = 2
+v.opt.tabstop = 4
+
